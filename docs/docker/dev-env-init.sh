338dbe8aeb1bc9592fbeca0804cc34acc860fce5
liaofushen <liaofushen@foxmail>
2022-10-15 13:36:59 +0800

    remove module docs

--- docs/docker/dev-env-init.sh
+++
@@ -1,8 +0,0 @@
-FROM centos
-RUN yum -y install wget \
-    && wget -O redis.tar.gz "http://download.redis.io/releases/redis-5.0.4.tar.gz" \
-    && tar -xvf redis.tar.gz
-    && cd ./redis
-    && make
-    && cd ..
-CMD ["sh", "-c", "./redis/redis-server"]
