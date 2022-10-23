568bc4d1d59eed2d9625aadd266ef7c99e4c9cda
liaofushen <liaofushen@foxmail>
2022-10-23 20:38:46 +0800

    update: fix mvn

--- build.cmd
+++ build.cmd
@@ -1,7 +1,11 @@
 call C:\home\softwares\apache-maven-3.8.6\bin\mvn.cmd -f pom.xml clean package -P prod -D maven.test.skip=true
 
-cd ./tecwealth-web
+cd .\tecwealth-web
 mkdir .\target\tecwealth-web
+mkdir .\target\tecwealth-web\logs
+mkdir .\target\tecwealth-web\config
 xcopy .\target\*.jar .\target\tecwealth-web\
 xcopy .\script\* .\target\tecwealth-web\
+xcopy .\target\classes\*.yml .\target\tecwealth-web\config
+xcopy .\target\classes\*.xml .\target\tecwealth-web\config
 cd ..
--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ mvn clean package -P prod -D  maven.test.skip=true
 cd ./tecwealth-web
 mkdir ./target/tecwealth-web
 mkdir ./target/tecwealth-web/logs
+mkdir ./target/tecwealth-web/config
 cp -rf ./target/*.jar ./target/tecwealth-web
 cp -rf ./script/* ./target/tecwealth-web
+cp -rf ./target/classes/*.yml ./target/tecwealth-web/config
+cp -rf ./target/classes/*.xml ./target/tecwealth-web/config
 cd -
